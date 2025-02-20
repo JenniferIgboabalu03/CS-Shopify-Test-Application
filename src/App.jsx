c2f0f539615a9133fb39a38a6d8ea21a0cf77e1a
JenniferIgboabalu03 <jenniferigboabalu@gmail.com>
2025-02-20 02:56:06 +0100

    changed somethings

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,11 @@
 import { Home } from "./Home Page/home"
-import { BrowserRouter as Route, Router, Routes  } from "react-router"
 
 function App() {
 
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route path='/' element={<Home/> } />
-        </Routes>
-      </div>
-    </Router>
-    
+    <div>
+      <Home/>
+    </div>
   )
 }
 
