941b0b1407e1c3c531fe50f336aca51679ef8660
JenniferIgboabalu03 <jenniferigboabalu@gmail.com>
2025-02-20 02:48:43 +0100

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import { Home } from "./Home Page/home"
+import { BrowserRouter as Route, Router, Routes  } from "react-router"
 
 function App() {
 
   return (
-    <div>
-      <Home/>
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route path='/' element={<Home/> } />
+        </Routes>
+      </div>
+    </Router>
+    
   )
 }
 
